610a46283d43a6e624838aecbe7efeec3eb79458
ravi <ravibhuva577@gmail.com>
2023-02-03 21:11:55 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Nav from './components/Nav';
+import Card from './components/Card';
+
+function App() {
+  return (
+    <>
+    <Nav/>
+    <Card/>
+    <Card/>
+    <Card/>
+    </>
+    
+  );
+}
+
+export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div class="card-group">
+  <div class="card">
+    <img class="card-img-top" src="https://images.unsplash.com/photo-1524492412937-b28074a5d7da?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80" alt="Card image cap"/>
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="https://www.itchotels.com/content/dam/itchotels/in/umbrella/itc/hotels-listing/hotels-listing-card/itc-royal-bengal.png" alt="Card image cap"/>
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="https://www.india.com/wp-content/uploads/2021/02/TheLeelaJaipur.jpeg" alt="Card image cap"/>
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+</div>
+  )
+}
+
+export default Card
