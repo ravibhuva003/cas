26d9c7b1433bac74c0b6ed1bc8783c0fb5fe96f6
Mayank <mayanksheladiya49@gmail.com>
2023-02-05 12:03:24 +0530

    gvgvgj

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,37 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import axios from 'axios';
 function Nav() {
+  var [que,setQue]=useState("");
+  var [obj,setObj]=useState({});
+
+  function changeState(e){
+     setQue(e.target.value);
+  }
+
+  function FetchData(e){
+    e.preventDefault();
+    console.log("fetch data called");
+   
+    var [a,b]=que.split(" ");
+    console.log(a,b);
+    axios
+    .get(`http://api.weatherapi.com/v1/current.json?key=e4a290301fa444a6bd754423230502&q=${a}`)
+    .then(
+      (e)=>{
+        console.log(e);
+      setObj({...obj,first:e,});
+    })
+    axios
+    .get(`http://api.weatherapi.com/v1/current.json?key=e4a290301fa444a6bd754423230502&q=${b}`)
+    .then(
+      (e)=>{
+        console.log(e);
+      setObj({...obj,last:e});
+    })
+    console.log(obj);
+   
+  }
+
   return (
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
@@ -19,8 +50,8 @@ function Nav() {
         <a class="nav-link" href="#">Contact US</a>
       </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+    <form class="form-inline my-2 my-lg-0" onSubmit={FetchData}>
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" onChange={changeState}/>
       <button class="btn btn-outline-warning my-2 my-sm-0" type="submit">Search</button>
     </form>
   </div>
